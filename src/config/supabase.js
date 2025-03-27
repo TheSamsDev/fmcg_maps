43496bc17e940955ac2a7ff2a2bdb021878514dd
samsdev-ai <workspacesam174@gmail.com>
2025-03-27 23:38:06 +0500

    key cahge

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || "https://zpzcpbgijmpqminpbnqz.supabase.co"
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpwemNwYmdpam1wcW1pbnBibnF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4NTY3NzksImV4cCI6MjA1ODQzMjc3OX0.1SRpX07bd31OLzzKbY46nRfHb96bM_AJJ6yFUMdDTkg"
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error('Missing Supabase environment variables. Please check your .env file')
